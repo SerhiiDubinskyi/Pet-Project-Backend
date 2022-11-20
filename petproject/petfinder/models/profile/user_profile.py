3cdc44407d19b1bee39630fcd2977b778b391a11
serhii <kolobroad1@gmail.com>
2022-11-20 12:29:45 +0200

    added ABCProfile, UserProfile, Base(Append)

--- petproject/petfinder/models/profile/user_profile.py
+++ petproject/petfinder/models/profile/user_profile.py
@@ -4,4 +4,8 @@ from abc_profile import ABCProfile
 
 class UserProfile(ABCProfile):
     email = models.EmailField(max_length=254, blank=False, null=False)
-    phone = models.CharField(max_length=30, blank=False, null=False) # TODO : need validation
+    phone = models.CharField(max_length=30, blank=False, null=False)  # TODO : need validation
+    telegram = models.CharField(max_length=30, null=False)  # TODO : need validation
+    facebook = models.CharField(max_length=30, null=False)  # TODO : need validation
+    instagram = models.CharField(max_length=30, null=False)  # TODO : need validation
+    description = models.TextField(max_length=1000, blank=True, null=False)
